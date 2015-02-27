4ad6c7cf5c0f90431718c7e3d27a5b1d816303ae
Jerol Adorable <jerol2k3@gmail.com>
2015-02-27 11:27:43 +0800

    Debug Sendgrid
    Debug Send Mail notification

--- User.php
+++ User.php
@@ -152,7 +152,9 @@ class User {
 	public function sendnotification($email, $ipaddress, $timelock){
 
         $sendgridusername = getenv("SENDGRID_USERNAME");
+        var_dump($sendgridusername);
         $sendgridpassword = getenv("SENDGRID_USERNAME");
+        var_dump($sendgridpassword);
 
         $sendgrid = new SendGrid($sendgridusername, $sendgridpassword);
 
@@ -165,5 +167,6 @@ class User {
                 ' has been locked out for about ' . $timelock .
                 ' using ip address ' . $ipaddress . ' !</strong>');
         $response = $sendgrid->send($message);
+        var_dump($response);
 	}
 }
